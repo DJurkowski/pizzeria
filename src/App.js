732528610f1d785e7bf756f31eb0625166c33873
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-04-16 17:40:28 +0200

    Add globalstyle file into project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './components/Routing';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
