0f02048f998aef79e43518c04949837ee3f05fa2
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-04-16 19:41:04 +0200

    Delete navbar component from Routing

--- src/components/Routing/index.js
+++ src/components/Routing/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../../pages/Home';
-import Navbar from '../Navbar';
 
 const Routes = () => {
     return (
         <>
-            <Navbar />
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
